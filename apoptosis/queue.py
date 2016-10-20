49a37c64b58a14c4e278edfd8f198d4b2b144661
Franky Saken <frankysaken@gmail.com>
2016-10-20 22:34:19 +0000

    extend queue with recurring tasks

--- apoptosis/queue.py
+++ apoptosis/queue.py
@@ -3,12 +3,36 @@ import tornado.gen
 
 import itertools
 
+import uuid
 
-async def loop():
-    for x in itertools.count():
-        print("minute: {}".format(x))
-        await tornado.gen.sleep(60)
 
+_tasks = []
+
+
+async def _recurring(key, interval, f, *args, **kwargs):
+    while True:
+        if not key in _tasks:
+            return
+
+        f(*args, **kwargs)
+        await tornado.gen.sleep(interval)
+
+
+def add_once(f, *args, **kwargs):
+    tornado.ioloop.IOLoop.current().spawn_callback(f, *args, **kwargs)
+
+def add_recurring(interval, f, *args, **kwargs):
+    key = uuid.uuid4()
+
+    _tasks.append(key)
+
+    tornado.ioloop.IOLoop.current().spawn_callback(_recurring, key, interval, f, *args, **kwargs)
+
+    return key
+
+def stop_recurring(key):
+    _tasks.remove(key)
 
 def setup():
-    tornado.ioloop.IOLoop.current().spawn_callback(loop)
+    add_once(lambda x: print(x), 1)
+    add_recurring(60, lambda x: print(x), 1)
